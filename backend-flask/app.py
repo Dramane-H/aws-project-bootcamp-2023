bc8ad4b8d1272828764b47f611417ae536dca3e2
Dramane-H <hidramane@yahoo.fr>
2023-03-09 13:24:10 +0000

    updating dockercompose

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -118,7 +118,7 @@ if __name__ == "__main__":
 
 
 
-  from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
 xray_url = os.getenv("AWS_XRAY_URL")
